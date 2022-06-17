a6c49bfda59d44a36404efca09f6b02ad43ee575
RealSadLemon <esmodearburk@gmail.com>
2022-06-17 16:18:49 -0500

    Created action creators for clockwise and counter Clockwise and set up wheel switch statement in reducer

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Wheel(props) {
+function Wheel(props) {
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className="cog active" style={{ "--i": 0 }} id="0">B</div>
+        <div className="cog" style={{ "--i": 1 }} id="1"></div>
+        <div className="cog" style={{ "--i": 2 }} id="2"></div>
+        <div className="cog" style={{ "--i": 3 }} id="3"></div>
+        <div className="cog" style={{ "--i": 4 }} id="4"></div>
+        <div className="cog" style={{ "--i": 5 }} id="5"></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" >Counter clockwise</button>
@@ -18,3 +19,11 @@ export default function Wheel(props) {
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    wheel: state.wheel
+  }
+}
+
+export default connect(mapStateToProps, {})(Wheel);
--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,10 +1,18 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE } from './action-types'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
-}
+  switch(action.type) {
+    case MOVE_CLOCKWISE:
+      return state + 1;
+    case MOVE_COUNTERCLOCKWISE:
+      return state - 1; 
+    default:
+      return state;
+  };
+};
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
